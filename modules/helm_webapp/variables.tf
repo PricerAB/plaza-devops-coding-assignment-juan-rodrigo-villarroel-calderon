ce03c06ab6fa6fbdc75b5df2f2d2ef22306a9fbf
Juan Villarroel <villarroel24kyle@gmail.com>
2026-09-08 18:34:41 -0400

    added terraform requirements

--- modules/helm_webapp/variables.tf
+++ modules/helm_webapp/variables.tf
@@ -1,1 +1,26 @@
-# Add variables here
+variable "helm_release_name" {
+  description = "Name of the Helm release"
+  type        = string
+}
+
+variable "namespace" {
+  description = "Kubernetes namespace"
+  type        = string
+}
+
+variable "image_repository" {
+  description = "Docker image repository"
+  type        = string
+}
+
+variable "image_tag" {
+  description = "Docker image tag"
+  type        = string
+  default     = "latest"
+}
+
+variable "hello_world_message" {
+  description = "Message for the hello world endpoint"
+  type        = string
+  default     = "Hello, World!"
+}
