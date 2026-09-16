c2989e3379fe0f7f1d24faf9f4d13ddfd5bf165b
Juan Villarroel <villarroel24kyle@gmail.com>
2026-09-16 07:21:26 -0400

    fix terraform point to chart

--- modules/helm_webapp/main.tf
+++ modules/helm_webapp/main.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "webapp" {
   name             = var.helm_release_name
-  chart            = "${path.module}/../../helm-chart"
+  chart            = "${path.module}/../../helm-chart/webapp/"
   namespace        = var.namespace
   create_namespace = true
 
